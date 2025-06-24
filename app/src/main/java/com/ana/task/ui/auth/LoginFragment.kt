5efc1775557105fb963db2d3c6c8dcbbaa2dc332
Chutulio <tulio.2003@hotmail.com>
2025-06-24 17:43:26 -0300

    1

---
+++ app/src/main/java/com/ana/task/ui/auth/LoginFragment.kt
@@ -0,0 +1,29 @@
+package com.ana.task.ui.auth
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ana.task.R
+import com.ana.task.databinding.FragmentRegisterBinding
+
+
+class LoginFragment : Fragment() {
+
+    private var _binding: FragmentRegisterBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentRegisterBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
